1040e92d6a24caf0ddff1b4fbc7230de08ff741a
Hari Sharma <harishar@umich.edu>
2021-02-17 21:54:23 -0500

    Tweaked code

--- EmployeeSQL/employee_db_tables.sql
+++ EmployeeSQL/employee_db_tables.sql
@@ -15,8 +15,10 @@ CREATE TABLE employees (
 	last_name VARCHAR NOT NULL,
 	sex VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(emp_title_id)
 );
+
 
 CREATE TABLE departments(
 	dept_no VARCHAR NOT NULL,
@@ -27,25 +29,32 @@ CREATE TABLE departments(
 
 CREATE TABLE dept_emp (
   	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
+
 CREATE TABLE dept_manager (
   	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+
 CREATE TABLE salaries (
   	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   	salary INT NOT NULL
 );
 
+
 CREATE TABLE titles (
   	emp_title_id VARCHAR NOT NULL,
-  	title VARCHAR NOT NULL
+  	title VARCHAR NOT NULL,
+	PRIMARY KEY (emp_title_id)
 );
